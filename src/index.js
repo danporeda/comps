8df9df24ba6d65a91542dc551c6e78b423f6eaf6
Dan <danporeda@yahoo.com>
2024-04-18 15:43:18 -0400

    project boilerplate

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const el = document.getElementById('root');
+ReactDOM.createRoot(el);
+
+ShadowRoot.render(<App />);
