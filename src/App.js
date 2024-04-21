e5c96f9bf8ebd1a8a5683f1cbc9116d2f3a2ec03
Dan <danporeda@yahoo.com>
2024-04-21 17:35:44 -0400

    completed Button css prop class values. added ...rest to props for button element events

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
 import Button from './Button';
 
 function App() {
+
   return (
     <div>
       <div>
-        <Button success rounded outline>Penis Pop</Button>
+        <Button success rounded outline><GoBell /> Penis Pop</Button>
       </div>
       <div>
-        <Button danger outline>Titty Top</Button>
+        <Button danger outline><GoCloudDownload /> Titty Top</Button>
       </div>
       <div>
-        <Button warning>Clitty Cop</Button>
+        <Button warning><GoDatabase /> Clitty Cop</Button>
       </div>
       <div>
         <Button secondary outline>Bitty Bop</Button>
