d9d5dc3a450bac0779a057846586e1853163de99
Dan <danporeda@yahoo.com>
2024-04-21 18:14:45 -0400

    added /components and /pages to src. copied App.js and pasted into ButtonPage.js

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -1,7 +1,7 @@
 import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
-import Button from './components/Button';
+import Button from '../components/Button';
 
-function App() {
+function ButtonPage() {
 
   return (
     <div>
@@ -24,4 +24,4 @@ function App() {
   )
 }
 
-export default App;
+export default ButtonPage;
