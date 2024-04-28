0a40b44a56d45ca8dd887d086fd08cb47ae8b6db
Dan <danporeda@yahoo.com>
2024-04-28 19:26:58 -0400

    implemented icon logic into Accordion component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,12 +3,23 @@ import { useState } from 'react';
 function Accordion({ items }) {
 const [expandedIndex, setExpandedIndex] = useState(0);
 
+const handleClick = (nextIndex) => {
+  setExpandedIndex(nextIndex);
+}
+
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
+    const icon = <span>
+      {isExpanded ? 'DOWN' : 'LEFT'}
+    </span>;
+
       return (
         <div key={item.id}>
-          <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+          <div onClick={() => handleClick(index)}>
+            {icon}
+            {item.label}
+          </div>
           {isExpanded && <div>{item.content}</div>}
         </div>
       )
