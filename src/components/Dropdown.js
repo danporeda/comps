657b8040c7155e5be6d974354f95ef22b3619f58
Dan <danporeda@yahoo.com>
2024-04-29 14:08:06 -0400

    functional Dropdown component: state for selectedOption and menuOpen, click handlers for menu bar and for option displays. included icons and some styling for menu bar

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,40 @@
-function Dropdown() {
-  return <div>Dropdown...</div>
+import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go';
+
+function Dropdown({ options }) {
+  const [selectedOption, setSelectedOption] = useState('Select...');
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleOptionClick = (label) => {
+    setSelectedOption(label);
+    setMenuOpen(false);
+  }
+
+  const handleMenuClick = () => {
+    setMenuOpen(!menuOpen);
+  }
+
+  const renderedOptions = options.map((option) => {
+    return (
+      <div onClick={() => handleOptionClick(option.label)}>
+        {option.value}
+      </div>
+    )
+  })
+  
+  const icon = <span className="text-2xl">
+    {menuOpen ? <GoChevronDown /> : <GoChevronLeft />}
+  </span>
+
+  return (
+    <div>
+      <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={() => handleMenuClick()}>
+        {selectedOption}
+        {icon}
+      </div>
+      <div>{menuOpen && renderedOptions}</div>
+    </div>
+  )
 }
 
 export default Dropdown;
