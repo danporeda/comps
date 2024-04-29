5a28102baab0495b66f0edcec06c4b6c163d28ac
Dan <danporeda@yahoo.com>
2024-04-29 12:59:03 -0400

    start of next component: Dropdown. created /components/Dropdown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
 
 function App() {
-  const items = [
-    {
-      id: '3jh32',
-      label: 'You Can Dance?',
-      content: '...If you want to.'
-    },
-    {
-      id: '3jferf',
-      label: 'You Can Leave?',
-      content: '...Your friends behind.'
-    },
-    {
-      id: 'fffe32',
-      label: 'Cuz if your friends dont dance?',
-      content: '...Then theyre no friends of mine'
-    }
-  ];
-
-  return <Accordion items={items} />;
+  return <Dropdown />;
 }
 
 export default App;
---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,25 @@
+import Accordion from '../components/Accordion';
+
+function AccordionPage() {
+  const items = [
+    {
+      id: '3jh32',
+      label: 'You Can Dance?',
+      content: '...If you want to.'
+    },
+    {
+      id: '3jferf',
+      label: 'You Can Leave?',
+      content: '...Your friends behind.'
+    },
+    {
+      id: 'fffe32',
+      label: 'Cuz if your friends dont dance?',
+      content: '...Then theyre no friends of mine'
+    }
+  ];
+
+  return <Accordion items={items} />;
+}
+
+export default AccordionPage;
