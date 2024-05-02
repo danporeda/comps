2a5bd13130157ee18668fc827da557b493ea2472
Dan <danporeda@yahoo.com>
2024-05-02 17:02:35 -0400

    updated App component to display Sidebar and the page according to currentPath. added activeClassName prop to Link component to bold SideBar Link that is displaying

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -6,19 +6,19 @@ function ButtonPage() {
   return (
     <div>
       <div>
-        <Button success rounded outline className="mb-5"><GoBell /> Penis Pop</Button>
+        <Button success rounded outline className="mb-5"><GoBell /> Oh No!</Button>
       </div>
       <div>
-        <Button danger outline><GoCloudDownload /> Titty Top</Button>
+        <Button danger outline><GoCloudDownload /> Wuh Oh</Button>
       </div>
       <div>
-        <Button warning><GoDatabase /> Clitty Cop</Button>
+        <Button warning><GoDatabase /> Bitty Bitty Bop</Button>
       </div>
       <div>
-        <Button secondary outline>Bitty Bop</Button>
+        <Button secondary outline>Bing Bong</Button>
       </div>
       <div>
-        <Button primary rounded>Shitty Shop</Button>
+        <Button primary rounded>Ding Dang</Button>
       </div>
     </div>
   )
