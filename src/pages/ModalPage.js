b7051421db7bfb6ea55273ddb8b3a3433e584657
Dan <danporeda@yahoo.com>
2024-05-04 20:53:11 -0400

    added button to ModalPage to show Modal component, and showModal state

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
+import Button from '../components/Button';
 import Modal from '../components/Modal';
 
 function ModalPage() {
-  return <div>
-    <Modal />
-  </div>;
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  return (
+    <div>
+      <Button primary onClick={handleClick}>Open Modal</Button>
+      {showModal && <Modal />}
+    </div>
+  );
 }
 
 export default ModalPage;
