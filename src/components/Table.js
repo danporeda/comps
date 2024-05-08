b0bd35ec47fcc79682aec6bffac4c08245baa245
Dan <danporeda@yahoo.com>
2024-05-07 21:29:42 -0400

    created tabel props of data and config. Table component returns a table with data

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,32 @@
-function Table() {
-  return <div>Table</div>;
+function Table({ data, config }) {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+
+  const renderedRows = data.map((row) => {
+    return (
+      <tr className='border-b' key={row.name}>
+        <td className='p-3'>{row.name}</td>
+        <td className='p-3'>
+          <div className={`p-3 m-2 ${row.color}`}></div>
+        </td>
+        <td className='p-3'>{row.score}</td>
+      </tr>
+    )
+  })
+
+  return (
+    <table className='table-auto border-spacing-2'>
+      <thead>
+        <tr className='border-b2'>
+          {renderedHeaders}
+        </tr>
+      </thead>
+      <tbody>
+        {renderedRows}
+      </tbody>
+    </table>
+  )
 }
 
 export default Table;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,8 +1,30 @@
 import Table from '../components/Table';
 
 function TablePage() {
+  const data = [
+    { name: 'Orange', color: 'bg-orange-500', score: 5 },
+    { name: 'Apple', color: 'bg-red-500', score: 3 },
+    { name: 'Banana', color: 'bg-yellow-300', score: 1 },
+    { name: 'Lime', color: 'bg-green-400', score: 4 },
+  ];
+
+  const config = [
+    { 
+      label: 'Fruit',
+      render: (fruit) => fruit.name 
+    },
+    { 
+      label: 'Color',
+      render: (fruit) => fruit.color
+    },
+    { 
+      label: 'Score',
+      render: (fruit) => fruit.score
+    },
+  ]
+
   return <div>
-    <Table />
+    <Table data={data} config={config} />
   </div>;
 }
 
