5bf85ad9a6fadec86080951ab663ad782e0faf1d
Dan <danporeda@yahoo.com>
2024-05-09 18:41:47 -0400

    refactored handleClick of headers with first if-statement to detect if a new label is being clicked, to render an ascending order

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -8,6 +8,12 @@ function SortableTable(props) {
   const { config, data } = props;
 
   const handleClick = (label) => {
+    if (sortBy && label !== sortBy) {
+      setSortOrder('asc');
+      setsortBy(label);
+      return;
+    }
+
     if (sortOrder === null) {
       setSortOrder('asc');
       setsortBy(label);
