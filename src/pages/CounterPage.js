0fab15ba41538a1c990323613054e5fdfb5d97e1
Dan <danporeda@yahoo.com>
2024-05-10 20:24:18 -0400

    refactoring Counter; replaced useCounter hook with useState, incremment and decrement buttons

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,13 +1,24 @@
-import useCounter from '../hooks/use-counter';
+import { useState } from 'react';
 import Button from '../components/Button';
 
 function CounterPage({ initialCount }) {
-  const { count, increment } = useCounter(initialCount);
+  const [count, setCount] = useState(initialCount);
+
+  const increment = () => {
+    setCount(count + 1)
+  };
+
+  const decrement = () => {
+    setCount(count -1)
+  };
 
   return <div>
     <h1>Count is {count}</h1>
-    <Button className="primary" onClick={increment}>
+    <Button primary onClick={increment}>
       increment
+    </Button>
+    <Button danger onClick={decrement}>
+      decrement
     </Button>
   </div>
 };
