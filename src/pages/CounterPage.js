394f83269ed7090ac5f3b95567d97c2adeba0cd1
Dan <danporeda@yahoo.com>
2024-05-12 10:34:24 -0400

    completed counterPage with useState to handle count and valueToAdd, buttons and form functional

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import Button from '../components/Button';
+import Panel from '../components/Panel';
 
 function CounterPage({ initialCount }) {
   const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
 
   const increment = () => {
     setCount(count + 1)
@@ -12,15 +14,41 @@ function CounterPage({ initialCount }) {
     setCount(count -1)
   };
 
-  return <div>
-    <h1>Count is {count}</h1>
-    <Button primary onClick={increment}>
-      increment
-    </Button>
-    <Button danger onClick={decrement}>
-      decrement
-    </Button>
-  </div>
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value) || 0;
+    setValueToAdd(value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCount(count + valueToAdd);
+    setValueToAdd(0);
+  }
+
+  return (
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {count}</h1>
+      <div className="flex flex-row">
+        <Button primary onClick={increment}>
+          increment
+        </Button>
+        <Button danger onClick={decrement}>
+          decrement
+        </Button>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label>Add a Lot!</label>
+        <input 
+          value={valueToAdd || ''}
+          onChange={handleChange}
+          type="number" 
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <Button warning>Add it!</Button>
+      </form>
+    </Panel>
+  )
 };
 
 export default CounterPage;
